fd95106fdb82f33976bcf9aecf67a2020b9512b5
dirk_t <dirk_t@gmx.net>
2025-07-26 14:01:18 +0200

    replaced didkit with ssi lib

--- tests/test_log.rs
+++
@@ -1,7 +0,0 @@
-use noise_did_vc::logging::{log_event, AuditEvent};
-
-#[test]
-fn test_logging() {
-    let did = "did:example:alice";
-    log_event(AuditEvent::DidResolved { did: did.to_string() });
-}
